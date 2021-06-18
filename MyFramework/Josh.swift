3fdf2742dfa9a46d59a8401aacce4b3e6ad8fefe
Joshua Liebowitz <taquitos@users.noreply.github.com>
2021-06-18 11:52:10 -0700

    initial commit

---
+++ MyFramework/Josh.swift
@@ -0,0 +1,16 @@
+//
+//  Josh.swift
+//  MyFramework
+//
+//  Created by Joshua Liebowitz on 6/18/21.
+//
+
+import Foundation
+
+public class Josh {
+    
+    public class func start() -> String {
+        return "Started"
+    }
+    
+}
