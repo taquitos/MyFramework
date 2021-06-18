c191a6238faab19b9ad1ea80fb9953e6ad0dd9ad
Joshua Liebowitz <taquitos@users.noreply.github.com>
2021-06-18 12:03:43 -0700

    update platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MyFramework",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v9), .watchOS("6.2"), .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
